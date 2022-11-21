41212d1b592ec7cf4edb64c8053fc484c1bd21c1
DevelopAamir <88143370+DevelopAamir@users.noreply.github.com>
2022-11-21 19:22:56 +0545

    This is Front End.
    This is only resources to start working.

---
+++ lib/homepage.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:image/chatpage.dart';
+import 'comps/styles.dart';
+import 'comps/widgets.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool open  = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo.shade400,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo.shade400,
+        title: const Text('Flash Chat'),
+        elevation: 0,
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    open == true? open = false: open = true;
+                  });
+                },
+                icon:  Icon(
+                  open == true? Icons.close_rounded :Icons.search_rounded,
+                  size: 30,
+                )),
+          )
+        ],
+      ),
+      drawer: ChatWidgets.drawer(),
+      body: SafeArea(
+        child: Stack(
+          alignment: AlignmentDirectional.topEnd,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(0),
+                  child: Container(
+                    color: Colors.indigo.shade400,
+                    padding: const EdgeInsets.all(8),
+                    height: 160,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 10),
+                          child: Text(
+                            'Recent Users',
+                            style: Styles.h1(),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          height: 80,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, i) {
+                              return ChatWidgets.circleProfile();
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    decoration: Styles.friendsBox(),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          child: Text(
+                            'Contacts',
+                            style: Styles.h1().copyWith(color: Colors.indigo),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: ListView.builder(
+                              itemBuilder: (context, i) {
+                                return ChatWidgets.card(
+                                  title: 'John Doe',
+                                  subtitle: 'Hi, How are you !',
+                                  time: '04:40',
+                                  onTap: () {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) {
+                                          return const ChatPage(
+                                            id: '',
+                                          );
+                                        },
+                                      ),
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ChatWidgets.searchBar(open)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'homepage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      themeMode: ThemeMode.light,
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MyHomePage();
+  }
+}
